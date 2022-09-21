4b69ecdd0b1e2ca9273a2c1fc070f6665302eb7e
Odina <osaidnazarova>
2022-09-21 23:47:14 +0500

    homework

---
+++ homework.js
@@ -0,0 +1,67 @@
+// // Homework
+// let a = 2020;
+// console.log(2020 % 4);
+// a % 4 == 0 && console.log("Kabisa yili");
+// let b = 2019
+// console.log(2019 % 4);
+// a%4 == 0 && console.log("Kabisa yili emas");
+//  // homework
+// let string = "WebBr@in Ac@demy"
+// console.log(string.replace(/@/gi, a));
+// // Homework
+// let str = Ass#a###lamu al#e#kum
+// console.log(str.replace(/#/gi, ""));
+// let string = 'WebBrain Codingcamp'
+// console.log(string.length);
+// console.log(( string.length**0.5));                                                                                                                // // Task1
+// var a = fozil
+// console.log(a.endsWith(l));
+// var b = fozif
+// console.log(b.endsWith(l));
+// // Task2
+// var str1 = 'Web';
+// var str2 = 'Brain';
+// var str3 = str1
+// console.log(str1, str2);
+// var str1 = str2
+// var str2 = str3
+// console.log(str1, str2);
+
+// let x = 'hello Odina'
+// let y='a'
+// // x.includes(y) && console.log('bor');
+// // !(x.includes(y)) && console.log('yoq');
+
+// // x.search(y) && console.log('bor');
+// // !(x.search(y)) && console.log('yoq');
+
+// // let x = 'Chocolate'
+// // let d= 4
+// // // y = x.slice(x.length-d)
+// // // console.log(y.repeat(d));
+
+// // // let z= 'Chocolate'
+// // // y = z.slice(5, 9)
+// // // console.log(y.repeat(d));
+
+
+// // let container = document.getElementById('con')
+// // let  y= prompt('Write your name')
+// // let x = `<h1> Hello ${y} </h1>`
+// // container.innerHTML+=x
+
+// function E_g5() {
+//     return 2.71
+    
+// }
+// console.log(E_g5());
+
+
+// function pi_g5() {
+//     return 3.14
+    
+// }
+// console.log(pi_g5());
+
+// Homework task
+// math.floor math.ceil math.round math.abs shulani funksiyaga qo'yish
